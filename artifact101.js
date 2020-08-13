c15d6bcb1bd5cf01670c650e334a00359fe13424
myapp01 <69285962+myapp01@users.noreply.github.com>
2020-08-13 11:31:53 +0700

    Add files via upload

---
+++ artifact101.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+function App(){
+  return(
+  <View style={{
+    justifyContent:"center",
+    flex:1,
+    alignItems:"center" 
+    }}>
+      <text style={{color:"BLUE",fontSize:"20px",fontweight:"bold"}}>อยากกินหมูกรอบทอดกระเทียม</text>
+    </View>
+  )
+}
+export default App;       
